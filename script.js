e9260d6fdb3cd675f62251f6be88e7bb8a6f7ed3
janiseimuss <janis.eimuss@gmail.com>
2026-06-05 11:57:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,30 @@ const bounds = [[0, 0], [imageHeight, imageWidth]];
 L.imageOverlay('assets/map.jpg', bounds).addTo(map);
 map.fitBounds(bounds);
 
-function createIcon(colorClass) {
+function getSymbolHtml(symbolType) {
+  switch (symbolType) {
+    case 'tele2-bs':
+      return '<div class="symbol-circle-yellow"></div>';
+    case 'tele2-hub':
+      return '<div class="symbol-circle-purple"></div>';
+    case 'lvrtc-access':
+      return '<div class="symbol-triangle-bluegray"></div>';
+    case 'tet-pm':
+      return '<div class="symbol-triangle-red"></div>';
+    default:
+      return '<div class="symbol-circle-yellow"></div>';
+  }
+}
+
+function createSymbolIcon(symbolType) {
+  const isTriangle = symbolType === 'lvrtc-access' || symbolType === 'tet-pm';
+
   return L.divIcon({
     className: '',
-    html: `<div class="custom-marker ${colorClass}"></div>`,
-    iconSize: [18, 18],
-    iconAnchor: [9, 9]
+    html: getSymbolHtml(symbolType),
+    iconSize: isTriangle ? [22, 20] : [18, 18],
+    iconAnchor: isTriangle ? [11, 18] : [9, 9],
+    popupAnchor: [0, -10]
   });
 }
 
@@ -24,7 +42,8 @@ fetch(`data/markers.json?v=${Date.now()}`)
   .then(response => response.json())
   .then(points => {
     points.forEach(point => {
-      const icon = createIcon(point.icon || 'marker-red');
+      const symbolType = point.symbolType || 'tele2-bs';
+      const icon = createSymbolIcon(symbolType);
 
       L.marker([point.y, point.x], { icon })
         .addTo(map)
