dc65b75995f54e072cc51b28b7a1a29f072905d0
janiseimuss <janis.eimuss@gmail.com>
2026-06-05 13:35:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function getSymbolHtml(symbolType) {
     case 'lvrtc-access':
       return '<div class="symbol-triangle-bluegray"></div>';
     case 'tet-pm':
-      return '<div class="symbol-triangle-red"></div>';
+      return '<div class="symbol-square-red"></div>';
     default:
       return '<div class="symbol-circle-yellow"></div>';
   }
